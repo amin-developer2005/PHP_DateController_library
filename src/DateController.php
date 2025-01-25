6620e19619aa61dde7f79eb528aba1c7e212a8ff
mohamadamin meghdadi <mohamadamin.meghdadi@gmail.com>
2025-01-25 16:34:35 +0330

    Date Controller class Library version specified;

--- src/DateController.php
+++ src/DateController.php
@@ -3,6 +3,11 @@
 /**
  * Jalali & Gregorian DateController module class;
  * 
+ * An Object-Oriented PHP library for converting Gregorian dates to Jalali dates and vice versa.
+ * This library includes a DateController class that offers two main methods: one for converting Gregorian dates to Jalali dates, and another for converting Jalali dates to Gregorian dates.
+ * I've designed it to be easy to use into your PHP projects;
+ * 
+ * 
  * @author: Mohammadamin Meghdadi
  * @email: mohamadamin.meghdadi@gmail.com
  * @license: MIT
